0e7f9c68ef9afbb7fc72f848305de74d71c2d81b
ryan3040 <ryan3040@gmail.com>
2026-01-08 18:40:03 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import Expo from "expo-server-sdk";
+import {Expo} from "expo-server-sdk";
 
 const app = new Hono();
 const expo = new Expo();
